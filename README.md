be0fd62b8baec69c668d473d5899982b6d040391
Joy <joyxie1018@gmail.com>
2023-12-31 15:07:52 +0800

    init

--- README.md
+++
@@ -1,3 +0,0 @@
-# note-sth 讀書筆記
-
-[紀錄一些小東西](https://joyxie1018.github.io/note-sth/)
--- docs/.vitepress/configs/head.ts
+++
@@ -1,5 +0,0 @@
-import type { HeadConfig } from "vitepress";
-
-export const head: HeadConfig[] = [
-    ['link', { rel: "icon", href: "/note-sth/kirby_icon.svg"}],
-]
